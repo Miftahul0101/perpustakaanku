9ccd5c21920621514c07a0e6c05b45491e68fa82
Miftah0101 <miftahul.9680>
2025-01-30 11:04:10 +0700

    bukukategori

--- routes/web.php
+++ routes/web.php
@@ -52,13 +52,6 @@ Route::middleware(['auth', 'role:petugas'])->prefix('petugas')->group(function (
     Route::resource('buku', BukuController::class);
     Route::resource('kategori', KategoriController::class);
         
-        // View categories for a book
-        Route::get('buku/{buku}/kategori', 'getCategories')
-            ->name('buku.kategori.index');
-        
-        // View books by category
-        Route::get('kategori/{kategori}/books', 'getBooksByCategory')
-            ->name('kategori.books');
     });
 
 

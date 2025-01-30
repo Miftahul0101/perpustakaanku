a5a5b04f8e50984e44c2c6c47e0396c4e70c83d1
Miftah0101 <miftahul.9680>
2025-01-30 11:00:50 +0700

    bukukategori

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\BukuKategoriController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,18 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
     Route::apiResource('kategori', KategoriController::class);
     
-    // Buku-Kategori API Routes
-    Route::controller(BukuKategoriController::class)->group(function () {
-        // Assign categories to book
-        Route::post('buku/{buku}/kategori', 'assignCategories');
-        
-        // Remove category from book
-        Route::delete('buku/{buku}/kategori/{kategori}', 'removeCategory');
-        
-        // Get categories for a book
-        Route::get('buku/{buku}/kategori', 'getCategories');
-        
-        // Get books by category
-        Route::get('kategori/{kategori}/books', 'getBooksByCategory');
-    });
+    
 });
